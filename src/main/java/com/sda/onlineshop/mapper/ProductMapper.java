9a4089f791be337ae1dc539a80b024a38a09cc95
Vitor <vcsoouza@hotmail.com>
2023-03-23 21:06:46 +0200

    Implemented home page

--- src/main/java/com/sda/onlineshop/mapper/ProductMapper.java
+++ src/main/java/com/sda/onlineshop/mapper/ProductMapper.java
@@ -2,9 +2,11 @@ package com.sda.onlineshop.mapper;
 
 import com.sda.onlineshop.dto.ProductDto;
 import com.sda.onlineshop.entities.Product;
+import lombok.Builder;
 import org.springframework.stereotype.Component;
 
 @Component
+@Builder
 public class ProductMapper {
     public Product map(ProductDto productDto){
         return Product.builder()
@@ -12,8 +14,18 @@ public class ProductMapper {
                 .description(productDto.getDescription())
                 .name(productDto.getName())
                 .category(productDto.getCategory())
-                .unitsInStock(Integer.valueOf(productDto.getStock()))
+                .unitsInStock(Integer.valueOf(productDto.getUnitsInStock()))
                 .build();
 
+    }
+    public ProductDto map (Product product){
+        return ProductDto.builder()
+                .id(String.valueOf(product.getId()))
+                .price(String.valueOf(product.getPrice()))
+                .description(product.getDescription())
+                .name(product.getName())
+                .category(product.getCategory())
+                .unitsInStock(String.valueOf(product.getUnitsInStock()))
+                .build();
     }
 }
